53af1bed9d1e9c776c14ecc2e6c5de6f1da696bf
Thanh Ho <chithanh89dn@gmail.com>
2019-06-21 14:34:41 +0700

    Add check ssh connection option

--- view_log_v1.sh
+++ view_log_v1.sh
@@ -2,6 +2,14 @@
 remote_user="root"
 ssh_key="~/.ssh/id_rsa"
 read -p "Enter remote server IP: " remote_sv_ip
+if ssh -i $ssh_key $remote_user@$remote_sv_ip "true"
+  then
+    echo "$remote_sv_ip connect successful"
+  else
+    echo "$remote_sv_ip cannot connect"
+    exit 1
+fi
+
 PS3='Select an option and press Enter: '
 option1="/var/log/nginx/error.log"
 option2="/var/log/mongodb/mongod.log"
@@ -13,18 +21,18 @@ options=("$option1"
 select opt in "${options[@]}"
 do
   case $opt in
-        "$option1")
-          ssh -i $ssh_key $remote_user@$remote_sv_ip "tail -f $opt"
-          break
-          ;;
-        "$option2")
-          ssh -i $ssh_key $remote_user@$remote_sv_ip "tail -f $opt"
-          break
-          ;;
-        "$option3")
-          ssh -i $ssh_key $remote_user@$remote_sv_ip "tail -f $opt"
-          break
-          ;;
-        *) echo "invalid option";;
+    "$option1")
+    ssh -i $ssh_key $remote_user@$remote_sv_ip "tail -f $opt"
+    break
+    ;;
+    "$option2")
+    ssh -i $ssh_key $remote_user@$remote_sv_ip "tail -f $opt"
+    break
+    ;;
+    "$option3")
+    ssh -i $ssh_key $remote_user@$remote_sv_ip "tail -f $opt"
+    break
+    ;;
+    *) echo "invalid option";;
   esac
 done
