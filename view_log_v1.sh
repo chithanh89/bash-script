5f687d9424aefc9967e8fab3b785c71c29be379c
Thanh Ho <chithanh89dn@gmail.com>
2019-06-21 09:26:46 +0700

    Bash script to view logs of remote server

---
+++ view_log_v1.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+remote_user="root"
+ssh_key="~/.ssh/id_rsa"
+read -p "Enter remote server IP: " remote_sv_ip
+PS3='Select an option and press Enter: '
+option1="/var/log/nginx/error.log"
+option2="/var/log/mongodb/mongod.log"
+option3="/opt/tomcat/logs/catalina.out"
+options=("$option1"
+         "$option2"
+         "$option3")
+
+select opt in "${options[@]}"
+do
+  case $opt in
+        "$option1")
+          ssh -i $ssh_key $remote_user@$remote_sv_ip "tail -f $opt"
+          break
+          ;;
+        "$option2")
+          ssh -i $ssh_key $remote_user@$remote_sv_ip "tail -f $opt"
+          break
+          ;;
+        "$option2")
+          ssh -i $ssh_key $remote_user@$remote_sv_ip "tail -f $opt"
+          break
+          ;;
+        *) echo "invalid option";;
+  esac
+done
