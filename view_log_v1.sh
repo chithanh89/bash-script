47ba9bde7439e01c1ae7dcf2782bb45d287a4b35
Thanh Ho <chithanh89dn@gmail.com>
2019-07-03 10:11:21 +0700

    Update viewlog with sudo user

--- view_log_v1.sh
+++ view_log_v1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-remote_user="root"
+remote_user="ct"
 ssh_key="~/.ssh/id_rsa"
 read -p "Enter remote server IP: " remote_sv_ip
 if ssh -i $ssh_key $remote_user@$remote_sv_ip "true"
@@ -11,7 +11,7 @@ if ssh -i $ssh_key $remote_user@$remote_sv_ip "true"
 fi
 
 PS3='Select an option and press Enter: '
-option1="/var/log/nginx/error.log"
+option1="/var/log/syslog"
 option2="/var/log/mongodb/mongod.log"
 option3="/opt/tomcat/logs/catalina.out"
 options=("$option1"
@@ -22,15 +22,15 @@ select opt in "${options[@]}"
 do
   case $opt in
     "$option1")
-    ssh -i $ssh_key $remote_user@$remote_sv_ip "tail -f $opt"
+    ssh -i $ssh_key $remote_user@$remote_sv_ip "sudo tail -f $opt"
     break
     ;;
     "$option2")
-    ssh -i $ssh_key $remote_user@$remote_sv_ip "tail -f $opt"
+    ssh -i $ssh_key $remote_user@$remote_sv_ip "sudo tail -f $opt"
     break
     ;;
     "$option3")
-    ssh -i $ssh_key $remote_user@$remote_sv_ip "tail -f $opt"
+    ssh -i $ssh_key $remote_user@$remote_sv_ip "sudo tail -f $opt"
     break
     ;;
     *) echo "invalid option";;
