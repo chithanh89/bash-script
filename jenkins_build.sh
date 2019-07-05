bf42c662a0efaeb90697ceac27efe649826ccc00
Thanh Ho <chithanh89dn@gmail.com>
2019-07-05 16:47:31 +0700

    Update script with option

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -3,12 +3,25 @@ user="admin"
 token=""
 jenkins_url=""
 
-read -p "Enter jenkins jobs name: " jobs
-read -p "Enter tag of repos: " tag
-read -p "Enter env to build: " env
+PS3='Select an option and press Enter: '
+option1="athena"
+option2="driver-signup"
+option3="newcc-client"
+options=("$option1"
+         "$option2"
+         "$option3")
+select opt in "${options[@]}"
+do
+    case $opt in
+        *) reply=$REPLY
+        read -p "Enter tag of repos: " tag
+        read -p "Enter env to build: " env
+        break;
+    esac
+done
 
-curl -X POST "https://$user:$token@$jenkins_url/job/$jobs/buildWithParameters?TAG=$tag&ENV=$env"
+curl -X POST "https://$user:$token@$jenkins_url/job/$opt/buildWithParameters?TAG=$tag&ENV=$env"
 sleep 10
-status=$(curl --silent "https://$user:$token@$jenkins_url/job/$jobs/lastBuild/api/json")
+status=$(curl --silent "https://$user:$token@$jenkins_url/job/$opt/lastBuild/api/json")
 echo "$status"
 exit 1
