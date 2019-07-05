68cff929b05bf73f8a601a3135c2cfc17dc44cd2
Thanh Ho <chithanh89dn@gmail.com>
2019-07-05 09:59:32 +0700

    Fix missing  with url

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -7,8 +7,8 @@ read -p "Enter jenkins jobs name: " jobs
 read -p "Enter tag of repos: " tag
 read -p "Enter env to build: " env
 
-curl -X POST https://$user:$token@$jenkins_url/job/$jobs/buildWithParameters?TAG=$tag&ENV=$env
+curl -X POST "https://$user:$token@$jenkins_url/job/$jobs/buildWithParameters?TAG=$tag&ENV=$env"
 sleep 10
-status=$(curl --silent https://$user:$token@$jenkins_url/job/$jobs/lastBuild/api/json)
+status=$(curl --silent "https://$user:$token@$jenkins_url/job/$jobs/lastBuild/api/json")
 echo "$status"
 exit 1
