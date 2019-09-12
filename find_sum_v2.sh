405b2ef582b4622cbc0dffd159226f7cdf5b5460
Thanh Ho <chithanh89dn@gmail.com>
2019-09-12 15:18:47 +0700

    Fix error

--- find_sum_v2.sh
+++ find_sum_v2.sh
@@ -13,8 +13,7 @@ do
     case $f in
         *) reply=$REPLY
         select_folder=$(find $workdir/$version -type d -name "$f")
-        path=$(find $select_folder -type f -name "*.md")
- 	      find $path -type f -name "*.md" \( -exec echo {} \; -o -exec true \; \) -exec cat {} \;  > $workdir/sum-$version/$f-sum.txt
+ 	      find $select_folder -type f -name "*.md" \( -exec echo {} \; -o -exec true \; \) -exec cat {} \;  > $workdir/sum-$version/$f-sum.txt
     esac
 done
 exit 1 # remove when run by jenkins
