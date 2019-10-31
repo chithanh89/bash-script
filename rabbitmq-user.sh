d55b4fb4b1c1ada34fe90dd883e2d0d383a88ada
Thanh Ho <chithanh89dn@gmail.com>
2019-10-31 11:49:47 +0700

    Add rabbitmq create multi users script

---
+++ rabbitmq-user.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+users=("u1" "u2" "u3")
+pass=""
+tags=""
+for u in "${users[@]}"
+do
+    case $u in
+        *) reply=$REPLY
+        rabbitmqctl add_user $u $pass
+        sleep 2
+        rabbitmqctl set_user_tags $u $tags
+        sleep 2
+        rabbitmqctl set_permissions -p / $u ".*" ".*" ".*"
+        sleep 2
+    esac
+        rabbitmqctl list_users && rabbitmqctl list_user_permissions $u
+done
+exit 1 # remove when run by jenkins
